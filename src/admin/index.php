0874f9bbdecfe25d95daa0e1176499e4e46ee4c1
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 17:06:06 +0700

    Update: admin index now can load by it own | add more route

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,29 +1,62 @@
-<?php
-$view = isset($_GET['view']) ? $_GET['view'] : 'Dashboard';
+<!DOCTYPE html>
+<html lang="en">
 
-require_once 'views/components/SideNav.php';
-require_once 'views/components/Header.php';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="/assets/PetShop.svg" />
+    <title>PetShop Admin</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <script src="/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="https://kit.fontawesome.com/db7aab17a8.js" crossorigin="anonymous"></script>
+</head>
+<style>
+    body::-webkit-scrollbar {
+        display: none;
+    }
 
-switch ($view){
-    case 'Dashboard':
-        require_once 'views/dashboard.php';
-        break;
-    case 'products':
-        require_once 'views/admin/products.php';
-        break;
-    case 'categories':
-        require_once 'views/admin/categories.php';
-        break;
-    case 'orders':
-        require_once 'views/admin/orders.php';
-        break;
-    case 'users':
-        require_once 'views/admin/users.php';
-        break;
-    case 'settings':
-        require_once 'views/admin/settings.php';
-        break;
-    default:
-        require_once 'views/dashboard.php';
-        break;
-}
+    .headerDashboard {
+        width: 10%;
+    }
+</style>
+
+<body>
+    <?php
+    if (!isset($_COOKIE['loggedIn'])) {
+        header('Location: /');
+    }
+    $view = isset($_GET['view']) ? $_GET['view'] : 'Dashboard';
+    require_once 'views/components/Header.php';
+    require_once 'views/components/SideNav.php';
+    ?>
+    <div class=" vh-100 d-flex justify-content-center align-items-center">
+        <?php
+        switch ($view) {
+            case 'Dashboard':
+                require_once 'views/dashboard.php';
+                break;
+            case 'Pet_Categories':
+                require_once 'controllers/category/petCategory/list.php';
+                break;
+            case 'Product_Categories':
+                require_once 'controllers/category/productCategory/list.php';
+                break;
+            case 'Colors':
+                require_once 'controllers/color/list.php';
+                break;
+            case 'Sources':
+                require_once 'controllers/source/list.php';
+                break;
+            case 'Pets':
+                require_once 'controllers/pets/list.php';
+                break;
+            default:
+                require_once 'views/dashboard.php';
+                break;
+        }
+        ?>
+    </div>
+
+</body>
+
+</html>
