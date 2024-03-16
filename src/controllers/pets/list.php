e641f4b8172f9884f7da4402036797ac93811ce3
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 18:42:07 +0700

    Create: pet list controller for client

---
+++ src/controllers/pets/list.php
@@ -0,0 +1,9 @@
+<?php
+require_once "./models/pets.php";
+require_once './controllers/category/petCategory/list.php';
+require_once './controllers/color/list.php';
+require_once './controllers/source/list.php';
+require_once './models/petCategory.php';
+require_once './models/color.php';
+require_once './models/source.php';
+$listPet = getAllPet();
