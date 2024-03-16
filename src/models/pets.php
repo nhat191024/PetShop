e431cf08457d18fbde5bb2efcb40afc75c993658
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 18:43:10 +0700

    Create: pet model

---
+++ src/models/pets.php
@@ -0,0 +1,17 @@
+<?php
+require_once "pdo.php";
+function getPetByCategoryId($id) {
+    $sql = "SELECT * FROM pet WHERE category_id = $id"; 
+    return pdo_query($sql); 
+}
+
+function getPetById($id) {
+    $sql = "SELECT * FROM pet WHERE id = $id";
+    return pdo_query_one($sql);
+}
+
+function getAllPet() {
+    $sql = "SELECT * FROM pet";
+    return pdo_query($sql);
+}
+?>
