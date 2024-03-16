394a2f8785181e6a77ac07b7ea45554710290460
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 17:03:42 +0700

    Remove: category model and controller file

--- src/admin/controllers/category/list.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "models/category.php";
-
-$listCategory = get_category();
-
-include "views";
--- src/admin/models/category.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '/models/pdo.php';
-
-function get_category(){
-    $sql = "SELECT * FROM danhmuc";
-
-    
-}
