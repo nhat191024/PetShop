8ef1784c5532ebd1069d035905c006be6ca9722d
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 17:39:27 +0700

    Update: complete login auth function | info with save in cookie for now, will fix to session in future

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,15 +1,31 @@
 <?php
 require_once "../models/account.php";
 if (isset($_POST['loginBtn'])) {
-    $email = $_POST['logEmail'];
+    $username = $_POST['logUsername'];
     $password = $_POST['logPassword'];
-    loginAuth($email, $password);
+    loginAuth($username, $password);
 }
 
-function loginAuth($email, $password)
+function loginAuth($username, $password)
 {
-    $account = accountGetByEmail($email);
-    if(!$account){
-        header('Location: /');
+    $email = accountGetByEmail($username);
+    $user = accountGetByUsername($username);
+    if(!$email && !$user){
+        header('Location: /?loginFailed=true');
+    } else if($email || $user){
+        //! I has to use cookie instead of session because session is not working and i don't know why, i will fix it later (i hope so xD)
+        if($password == $email['password'] || $password == $user['password']){
+            setcookie('loggedIn', 'true', time() + 3600, '/');
+            if($user){
+                setcookie('username', $user['username'], time() + 3600, '/');
+                setcookie('role', $user['role'], time() + 3600, '/');
+            } else {
+                setcookie('username', $email['username'], time() + 3600, '/');
+                setcookie('role', $email['role'], time() + 3600, '/');
+            }
+            header('Location: /');
+        } else {
+            header('Location: /?loginFailed=true');
+        }
     }
 }
