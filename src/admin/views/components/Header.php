5427583c6d99ad834b719c016cd0b73af3669542
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 17:17:11 +0700

    Update: change header height and header not fixed anymore

--- src/admin/views/components/Header.php
+++ src/admin/views/components/Header.php
@@ -1,3 +1,3 @@
-<div class="h-50 w-100 bg-black d-flex justify-content-center">
+<div class="headerDashboard w-100 bg-black d-flex justify-content-center fixed-top">
     <span class="text-white fs-1 fw-semibold my-3"><?= $view ?></span>
 </div>
