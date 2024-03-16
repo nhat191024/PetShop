5fa8ae1b3359bad974b64fcbd76bee9c7a0cedb0
sharkvn1 <richberchannel01@gmail.com>
2024-03-16 18:42:38 +0700

    Create: account model

--- src/models/account.php
+++ src/models/account.php
@@ -1,5 +1,10 @@
 <?php 
 require_once "pdo.php";
+function accountGetByUsername($username ){
+    $sql = "SELECT * FROM account WHERE username = '$username'" ;
+    return pdo_query_one($sql);
+}
+
 function accountGetByEmail($email){
     $sql = "SELECT * FROM account WHERE email = '$email'";
     return pdo_query_one($sql);
