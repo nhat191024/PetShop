ae56e561253c88ac9f7ab548ab6f7b42606e8467
sharkvn1 <richberchannel01@gmail.com>
2024-03-21 11:24:18 +0700

    Update: change nav icon width and height

--- src/views/components/NavBar.php
+++ src/views/components/NavBar.php
@@ -1,7 +1,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary fs-5 px-5">
         <div class="container-fluid">
             <a class="navbar-brand " href="#">
-                <img src="/assets/PetShop.svg" alt="Logo" width="70" height="64" class="d-inline-block align-text-top">
+                <img src="/assets/PetShop.svg" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
