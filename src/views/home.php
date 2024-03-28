227751099b49ff569b1856015c7cad0d8033b050
FuminoLap <richberchannel01@gmail.com>
2024-03-28 12:57:26 +0700

    Update: add website welcome banner

--- src/views/home.php
+++ src/views/home.php
@@ -1,4 +1,13 @@
+<div class="mt-5 container-fluid d-flex flex-column justify-content-center align-items-center " style="height: 75vh">
+    <div class="text-center">
+        <h1>Welcome to PetShop</h1>
+        <h3>Where you can explore the adorable world of pets</h3>
+        <h3>Discover quality and unique products related to caring for and loving your four-legged family members 💖</h3>
+    </div>
+    <img src="../assets/banner3RemoveBg.png" alt="" class="w-50  h-75">
+</div>
 <div class="vh-100 container ">
+    <h2 class="text-center">Our Pets</h2>
     <div class="row row-cols-3 gap-2 justify-content-center mt-2">
         <?php foreach ($listPet as $x) { ?>
             <div class="card p-2 bg-body-tertiary" style="width: 18rem;">
