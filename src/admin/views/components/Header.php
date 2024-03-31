b955ef3b46f507514d2915d6bdb33f8b49dd8d85
FuminoLap <richberchannel01@gmail.com>
2024-03-31 19:58:47 +0700

    Update: change admin header bg color

--- src/admin/views/components/Header.php
+++ src/admin/views/components/Header.php
@@ -1,3 +1,3 @@
-<div class="headerDashboard w-100 bg-black d-flex justify-content-center fixed-top">
-    <span class="text-white fs-1 fw-semibold my-3"><?= $view ?></span>
+<div class="headerDashboard w-100 bg-info bg-gradient d-flex justify-content-center fixed-top">
+    <span class="text-black fs-1 fw-bold my-3"><?= $view ?></span>
 </div>
