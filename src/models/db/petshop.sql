c0cb8c1f72a6f1fd45da70db1b08069ecfa89665
FuminoLap <richberchannel01@gmail.com>
2024-04-03 22:26:57 +0700

    Update: new sql backup file

--- src/models/db/petshop.sql
+++ src/models/db/petshop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th3 21, 2024 lúc 02:17 AM
--- Phiên bản máy phục vụ: 8.0.31
--- Phiên bản PHP: 8.2.0
+-- Thời gian đã tạo: Th4 03, 2024 lúc 03:25 PM
+-- Phiên bản máy phục vụ: 8.2.0
+-- Phiên bản PHP: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE IF NOT EXISTS `account` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `password` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `password` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `role` tinyint NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
@@ -47,15 +47,63 @@ INSERT INTO `account` (`id`, `username`, `email`, `password`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `breed`
+--
+
+DROP TABLE IF EXISTS `breed`;
+CREATE TABLE IF NOT EXISTS `breed` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `breed`
+--
+
+INSERT INTO `breed` (`id`, `name`, `create_at`, `update_at`) VALUES
+(2, 'Shiba', '2024-04-03 02:10:53', '2024-04-03 02:10:53'),
+(3, 'Samoyed', '2024-04-03 05:34:47', '2024-04-03 05:34:47'),
+(4, 'Pit bull', '2024-04-03 05:35:04', '2024-04-03 05:35:04'),
+(5, 'Boerboel', '2024-04-03 05:42:42', '2024-04-03 05:42:42'),
+(6, 'Shepherd', '2024-04-03 05:43:37', '2024-04-03 05:43:37'),
+(7, 'Dobermann', '2024-04-03 05:45:24', '2024-04-03 05:45:24'),
+(8, 'Pomeranian', '2024-04-03 05:59:46', '2024-04-03 05:59:46'),
+(9, 'Pembroke Welsh Corgi', '2024-04-03 06:00:15', '2024-04-03 06:00:15'),
+(10, 'PhuQuoc', '2024-04-03 06:02:51', '2024-04-03 06:02:51'),
+(14, 'Japanese Chin', '2024-04-03 06:21:37', '2024-04-03 06:21:37');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `color`
 --
 
 DROP TABLE IF EXISTS `color`;
 CREATE TABLE IF NOT EXISTS `color` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `color`
@@ -64,7 +112,8 @@ CREATE TABLE IF NOT EXISTS `color` (
 INSERT INTO `color` (`id`, `name`) VALUES
 (1, 'red'),
 (2, 'white'),
-(5, 'yellow');
+(5, 'yellow'),
+(6, 'Brown');
 
 -- --------------------------------------------------------
 
@@ -75,31 +124,21 @@ INSERT INTO `color` (`id`, `name`) VALUES
 DROP TABLE IF EXISTS `pet`;
 CREATE TABLE IF NOT EXISTS `pet` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `price` int NOT NULL,
   `age` int NOT NULL,
   `gender` tinyint NOT NULL,
-  `source` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `source` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `breed_id` int NOT NULL,
   `color_id` int NOT NULL,
   `category_id` int NOT NULL,
-  `vaccination` varchar(20) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `quantity` int NOT NULL,
-  `note` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `vaccination` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `note` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `img_path` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
   `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `pet`
---
-
-INSERT INTO `pet` (`id`, `name`, `price`, `age`, `gender`, `source`, `color_id`, `category_id`, `vaccination`, `quantity`, `note`, `create_date`) VALUES
-(2, 'Dog nam', 120000, 2, 2, '1', 5, 1, 'vaccinated', 0, '', '2024-03-16 11:49:33'),
-(3, 'Dog nam', 120000, 2, 2, '1', 5, 2, 'vaccinated', 0, '', '2024-03-16 15:09:57'),
-(4, 'Dog nam', 143535, 3, 2, '1', 2, 3, 'not vaccinated', 0, '', '2024-03-16 15:10:15'),
-(5, 'Dog nam', 1351313, 1, 1, '1', 1, 1, 'vaccinated', 0, '', '2024-03-16 15:10:36'),
-(6, 'Dog nam', 213244, 2, 1, '1', 2, 1, 'vaccinated', 0, '', '2024-03-16 15:36:52'),
-(7, 'Dog nam', 214124, 2, 1, '1', 2, 2, 'vaccinated', 0, '', '2024-03-16 15:37:07');
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -110,7 +149,7 @@ INSERT INTO `pet` (`id`, `name`, `price`, `age`, `gender`, `source`, `color_id`,
 DROP TABLE IF EXISTS `pet_category`;
 CREATE TABLE IF NOT EXISTS `pet_category` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
@@ -126,22 +165,40 @@ INSERT INTO `pet_category` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `pet_schedule`
+--
+
+DROP TABLE IF EXISTS `pet_schedule`;
+CREATE TABLE IF NOT EXISTS `pet_schedule` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `pet_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `date` datetime NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `pet_source`
 --
 
 DROP TABLE IF EXISTS `pet_source`;
 CREATE TABLE IF NOT EXISTS `pet_source` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `pet_source`
 --
 
 INSERT INTO `pet_source` (`id`, `name`) VALUES
-(1, 'VietNam');
+(1, 'VietNam'),
+(3, 'German');
 
 -- --------------------------------------------------------
 
@@ -152,7 +209,7 @@ INSERT INTO `pet_source` (`id`, `name`) VALUES
 DROP TABLE IF EXISTS `product_category`;
 CREATE TABLE IF NOT EXISTS `product_category` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
