f9be0330d1c765d6f6a1b9bffc564d88e02e6291
FuminoLap <richberchannel01@gmail.com>
2024-04-03 18:54:42 +0700

    Update: add page feature for color

--- src/admin/controllers/color/list.php
+++ src/admin/controllers/color/list.php
@@ -1,6 +1,15 @@
 <?php
 require_once "./models/color.php";
 $listColor = getAllColor();
-if($view == "Colors"){
+if ($view == "Colors") {
+    $page = isset($_GET['page']) ? $_GET['page'] : 1; // Current page
+    $pageSize = 8; // number of breeds per page
+    $totalPage = ceil(count($listColor) / $pageSize); // Total page
+    if (isset($_GET['colorPrev']) && $page > 1) {
+        $page--;
+    } else if (isset($_GET['colorNext']) && $page < $totalPage) {
+        $page++;
+    }
+    $listColorPage = getAllColorByPage($page, $pageSize);
     require_once './views/color.php';
 }
--- src/admin/models/color.php
+++ src/admin/models/color.php
@@ -12,6 +12,13 @@ function getColorById($id)
     return pdo_query_one($sql);
 }
 
+function getAllColorByPage($page, $pageSize)
+{
+    $firstIndex = ($page - 1) * $pageSize;
+    $sql = "SELECT * FROM color LIMIT $firstIndex, $pageSize";
+    return pdo_query($sql);
+}
+
 function createColor($name)
 {
     $sql = "INSERT INTO color (name) VALUES ('$name')";
