694fc84aca51ec67e6922aef5abb5b22d1e4e015
FuminoLap <richberchannel01@gmail.com>
2024-04-03 18:56:35 +0700

    Update: remove controller require and change listPet to listPetPage

--- src/admin/controllers/pets/list.php
+++ src/admin/controllers/pets/list.php
@@ -1,22 +1,20 @@
 <?php
 require_once "./models/pets.php";
-require_once './controllers/category/petCategory/list.php';
-require_once './controllers/color/list.php';
-require_once './controllers/source/list.php';
 require_once './models/petCategory.php';
 require_once './models/color.php';
 require_once './models/source.php';
+require_once './models/breed.php';
 $listPet = getAllPet();
 if ($view == "Pets") {
     $page = isset($_GET['page']) ? $_GET['page'] : 1; // Current page
     $pageSize = 5; // number of pets per page
-    $totalPage = ceil(count(getAllPet()) / $pageSize); // Total page
+    $totalPage = ceil(count($listPet) / $pageSize); // Total page
     // Process the Prev and Next buttons
     if(isset($_GET['petsPrev']) && $page > 1) {
         $page--;
     } else if(isset($_GET['petsNext']) && $page < $totalPage) {
         $page++;
     }
-    $listPet = getAllPetByPage($page, $pageSize);
+    $listPetPage = getAllPetByPage($page, $pageSize);
     require_once './views/pet.php';
 }
