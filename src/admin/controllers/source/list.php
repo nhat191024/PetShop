d1ba9ee5087c97104ef105080976a3113c55b2ad
Fumino The Fish <richberchannel01@gmail.com>
2024-04-06 09:51:55 +0700

    Update: page control

--- src/admin/controllers/source/list.php
+++ src/admin/controllers/source/list.php
@@ -1,8 +1,13 @@
 <?php
 require_once "./models/source.php";
 $listSource = getAllSource();
-if($view == "Sources"){
+if ($view == "Sources") {
+    // Current page
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    // number of Items per page
+    $pageSize = 8;
+    // Total page
+    $totalPage = ceil(count($listProductCategory) / $pageSize);
+    $listSourcePage = getAllSourceByPage($page, $pageSize);
     require_once './views/source.php';
 }
-
-
