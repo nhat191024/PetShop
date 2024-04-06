c83525c034c018979bfb1fd1c6bf70425597518f
Fumino The Fish <richberchannel01@gmail.com>
2024-04-06 09:48:27 +0700

    Update: page control

--- src/admin/controllers/category/petCategory/list.php
+++ src/admin/controllers/category/petCategory/list.php
@@ -2,5 +2,9 @@
 require_once "./models/petCategory.php";
 $listPetCategory = getPetCategory();
 if($view == "Pet_Categories") {
+    $page = isset($_GET['page']) ? $_GET['page'] : 1; // Current page
+    $pageSize = 5; // number of pets per page
+    $totalPage = ceil(count($listPetCategory) / $pageSize); // Total page
+    $listPetCategoryPage = getPetCategoryByPage($page, $pageSize);
     require_once './views/petCategory.php';
 }
