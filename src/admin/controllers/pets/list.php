344d6fbf15750d8ab151513fd4393fc6c55af723
Fumino The Fish <richberchannel01@gmail.com>
2024-04-06 09:51:10 +0700

    Update: add comment and remove next - prev button control

--- src/admin/controllers/pets/list.php
+++ src/admin/controllers/pets/list.php
@@ -6,15 +6,12 @@ require_once './models/source.php';
 require_once './models/breed.php';
 $listPet = getAllPet();
 if ($view == "Pets") {
-    $page = isset($_GET['page']) ? $_GET['page'] : 1; // Current page
-    $pageSize = 5; // number of pets per page
-    $totalPage = ceil(count($listPet) / $pageSize); // Total page
-    // Process the Prev and Next buttons
-    if(isset($_GET['petsPrev']) && $page > 1) {
-        $page--;
-    } else if(isset($_GET['petsNext']) && $page < $totalPage) {
-        $page++;
-    }
+    // Current page
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    // number of Items per page
+    $pageSize = 5;
+    // Total page
+    $totalPage = ceil(count($listProductCategory) / $pageSize);
     $listPetPage = getAllPetByPage($page, $pageSize);
     require_once './views/pet.php';
 }
