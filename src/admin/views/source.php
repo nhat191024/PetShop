7183b046da3d293a1c598c76a78112442ad40c12
Fumino The Fish <richberchannel01@gmail.com>
2024-04-06 09:58:11 +0700

    Add: source view

--- src/admin/views/source.php
+++ src/admin/views/source.php
@@ -1,35 +1,71 @@
-<div class="m-5 w-50 d-flex flex-column align-items-end">
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary mb-2 w-25" data-bs-toggle="modal" data-bs-target="#add<?= $view ?>">
-        Add Source
-    </button>
-    <div class="w-100 border border-1 border-black rounded-2 p-1 d-flex">
-        <table class="table table-dark table-striped-columns text-center">
+<div class="container-fluid d-flex gap-2 p-3">
+    <div class="admin-side-container rounded-3">
+        <ol class="list-group list-group-numbered">
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                    <div class="fw-bold">Source</div>
+                    Total Source
+                </div>
+                <span class="badge text-bg-info text-white rounded-pill">
+                    <?= count($listSource); ?>
+                </span>
+            </li>
+        </ol>
+    </div>
+    <div class="admin-main-container bg-white p-2 rounded-3">
+    <div class="d-flex  justify-content-between px-5 pb-3">
+            <form class="d-flex w-25" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-info" type="submit">Search</button>
+            </form>
+            <button type="button" class="btn btn-outline-info fw-bold m-1 float-end" data-bs-toggle="modal" data-bs-target="#add<?= $view ?>">
+                + Add Source
+            </button>
+        </div>
+        <table class="table table-borderless table-striped text-center align-middle">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Action</th>
+                    <th scope="col" class="table-bg rounded-start-3">#</th>
+                    <th scope="col" class="table-bg">Source</th>
+                    <th scope="col" class="table-bg">Source ID</th>
+                    <th scope="col" class="table-bg rounded-end-3">Action</th>
                 </tr>
             </thead>
-            <tbody>
-                <?php foreach ($listSource as $key => $value) { ?>
+            <tbody class=" fw-medium">
+                <?php
+                foreach ($listSourcePage as $key => $x) { ?>
                     <tr>
-                        <th scope="row"><?= $key + 1 ?></th>
-                        <td scope="row"><?= $value['name'] ?></td>
-                        <td scope="row">
-                            <button class="btn btn-danger disabled">Deactivate</button>
-                            <button class="btn btn-warning">Edit</button>
-                            <a class="btn btn-info disabled">Details</a>
+                        <td><?= $key + 1 ?></td>
+                        <td><?= $x['name'] ?></td>
+                        <td><?= $x['id'] ?></td>
+                        <td style="width: 10%;" class="rounded-end-3">
+                            <button class="btn"><i class="fa-solid fa-eye" style="color: #74C0FC;"></i></button>
+                            <button class="btn"><i class="fa-solid fa-pen-to-square" style="color: #FFD43B;"></i></button>
+                            <button class="btn"><i class="fa-solid fa-trash" style="color: #ff0000;"></i></button>
                         </td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item <?php if ($currentPage <= 1) echo 'disabled' ?>">
+                    <a class="page-link" href="/admin/?view=<?= $view . "&page=" . $currentPage - 1 ?>">
+                        Previous
+                    </a>
+                </li>
+                <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
+                    <li class="page-item"><a class="page-link" href="/admin/?view=<?= $view . "&page=" . $i ?>"><?= $i ?></a></li>
+                <?php endfor; ?>
+                <li class="page-item <?php if ($currentPage == $totalPage) echo 'disabled' ?>">
+                    <a class="page-link" href="/admin/?view=<?= $view . "&page=" . $currentPage + 1 ?>">
+                        Next
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
-
 </div>
-
 <?php
 require_once "./views/components/addModal.php";
 ?>
