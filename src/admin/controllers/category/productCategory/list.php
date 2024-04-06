b8cde4d1b343db89d8d8d4cab51bf31719f1a7ae
Fumino The Fish <richberchannel01@gmail.com>
2024-04-06 09:49:34 +0700

    Update: page control

--- src/admin/controllers/category/productCategory/list.php
+++ src/admin/controllers/category/productCategory/list.php
@@ -2,5 +2,12 @@
 require_once "./models/productCategory.php";
 $listProductCategory = getProductCategory();
 if ($view == "Product_Categories") {
+    // Current page
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    // number of Items per page
+    $pageSize = 5;
+    // Total page
+    $totalPage = ceil(count($listProductCategory) / $pageSize);
+    $listProductCategoryPage = getProductCategoryByPage($page, $pageSize);
     require_once './views/productCategory.php';
 }
