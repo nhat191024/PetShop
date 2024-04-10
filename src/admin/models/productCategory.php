aa10594c43d460e85a6a1a488dc4874a00afcd4a
Fumino The Fish <richberchannel01@gmail.com>
2024-04-10 21:31:43 +0700

    Update: get by id and get by page function

--- src/admin/models/productCategory.php
+++ src/admin/models/productCategory.php
@@ -4,6 +4,18 @@ function getProductCategory() {
     $sql = "SELECT * FROM product_category";
     return pdo_query($sql);
 }
+
+function getProductCategoryById($id) {
+    $sql = "SELECT * FROM product_category WHERE id = $id";
+    return pdo_query_one($sql);
+}
+
+function getProductCategoryByPage($page, $pageSize) {
+    $firstIndex = ($page - 1) * $pageSize;
+    $sql = "SELECT * FROM product_category LIMIT $firstIndex, $pageSize";
+    return pdo_query($sql);
+}
+
 function createProductCategory($productCategoryName) {
     $sql = "INSERT INTO product_category (name) VALUES ('$productCategoryName')";
     pdo_execute($sql);
