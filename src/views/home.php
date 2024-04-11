dbe9f1f786673d4b348d7d4833c2791f1aea3092
Fumino The Fish <richberchannel01@gmail.com>
2024-04-11 11:26:33 +0700

    Update: in work process

--- src/views/home.php
+++ src/views/home.php
@@ -6,20 +6,46 @@
     </div>
     <img src="../assets/banner3RemoveBg.png" alt="" class="w-50  h-75">
 </div>
-<div class="vh-100 container">
+<div class="container">
     <h2 class="text-center text-info fw-bold">OUR PETS</h2>
-    <div class="d-flex gap-3 py-2  justify-content-center ">
-        <?php foreach($listPetCategory as $x) : ?>
-            <a href="" class="btn btn-outline-info"><?= $x['name'] ?></a>
-        <?php endforeach; ?>
-    </div>
     <div class="row row-cols-4 gap-3 justify-content-center">
         <?php foreach ($listPet as $x) { ?>
             <div class="card p-2 bg-white" style="width: 18rem;">
                 <a href="/?view=detail&details=<?= $x['id'] ?>" class="text-decoration-none">
-                    <img src="../assets/banner1.png" class="card-img-top rounded-2">
-                    <div class="card-body =">
-                        <h6 class="card-subtitle text-body-secondary"><?= getSourceById($x['source'])['name']  ?> <?= getPetCategoryById($x['category_id'])['name']  ?></h6>
+                    <div class="card-img-top rounded-2 text-center d-flex flex-column justify-content-center" style="height: 250px">
+                        <img src="<?= $x['img_path'] ?>" class="" style="width: 200px">
+                    </div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle text-body-secondary"><?= getSourceById($x['source_id'])['name']  ?> <?= getPetCategoryById($x['category_id'])['name']  ?></h6>
+                        <h5 class="card-title text-info"><?= $x['name'] ?></h5>
+                        <h6 class="mt-3 d-flex justify-content-between m-0">
+                            <span class=" text-info">
+                                <?php
+                                $a = strrev($x['price']);
+                                $b = str_split($a, 3);
+                                $c = implode(',', $b);
+                                $d = strrev($c);
+                                echo $d;
+                                ?>₫
+                            </span>
+                        </h6>
+                    </div>
+                </a>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+<div class="container mt-5">
+    <h2 class="text-center text-info fw-bold">OUR PRODUCT</h2>
+    <div class="row row-cols-4 gap-3 justify-content-center">
+        <?php foreach ($listPet as $x) { ?>
+            <div class="card p-2 bg-white" style="width: 18rem;">
+                <a href="/?view=detail&details=<?= $x['id'] ?>" class="text-decoration-none">
+                    <div class="card-img-top rounded-2 text-center d-flex flex-column justify-content-center" style="height: 250px">
+                        <img src="<?= $x['img_path'] ?>" class="" style="width: 200px">
+                    </div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle text-body-secondary"><?= getSourceById($x['source_id'])['name']  ?> <?= getPetCategoryById($x['category_id'])['name']  ?></h6>
                         <h5 class="card-title text-info"><?= $x['name'] ?></h5>
                         <h6 class="mt-3 d-flex justify-content-between m-0">
                             <span class=" text-info">
