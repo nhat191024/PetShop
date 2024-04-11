fd47b4292923df543d4213c0d0caa0b6653c1440
Fumino The Fish <richberchannel01@gmail.com>
2024-04-11 11:26:13 +0700

    Update: call controller

--- src/controllers/home.php
+++ src/controllers/home.php
@@ -1,4 +1,9 @@
 <?php 
+require_once "./models/pets.php";
+require_once './models/petCategory.php';
+require_once './models/color.php';
+require_once './models/source.php';
+require_once './controllers/pets/list.php';
 if($view == "/"){
     require_once "./views/home.php";
 }
