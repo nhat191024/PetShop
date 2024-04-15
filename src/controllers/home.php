3e6bb4f7165d937c53657a1967f3584a037ad9b6
FuminoLap <richberchannel01@gmail.com>
2024-04-15 21:37:03 +0700

    Update: require file

--- src/controllers/home.php
+++ src/controllers/home.php
@@ -3,6 +3,9 @@ require_once "./models/pets.php";
 require_once './models/petCategory.php';
 require_once './models/color.php';
 require_once './models/source.php';
+require_once './models/manufacturer.php';
+require_once './models/productCategory.php';
+require_once './controllers/product/list.php';
 require_once './controllers/pets/list.php';
 if($view == "/"){
     require_once "./views/home.php";
