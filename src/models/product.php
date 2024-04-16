e800a81cde6f2ccf5a5f6e9214d57438a3fc49e2
FuminoLap <richberchannel01@gmail.com>
2024-04-16 21:01:07 +0700

    Update: product

---
+++ src/models/product.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'pdo.php';
+
+function getAllProduct(){
+    $sql = "SELECT * FROM product";
+    return pdo_query($sql);
+}
+
+function getProductById($id){
+    $sql = "SELECT * FROM product WHERE id = $id";
+    return pdo_query_one($sql);
+}
