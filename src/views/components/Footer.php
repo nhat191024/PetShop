f3de86bcb1771eb65faff80d557a35f46aa1c482
Fumino The Fish <richberchannel01@gmail.com>
2024-04-19 00:43:52 +0700

    Add: footer

---
+++ src/views/components/Footer.php
@@ -0,0 +1,11 @@
+<footer class="d-flex p-4 align-items-center mt-5" style="width: 100%; height: 250px">
+    <div class="logo d-flex flex-column align-items-center">
+        <img src="assets/PetShop.png" alt="Website Logo" width="20%">
+        <h5 class="text-secondary fw-bold ">PetShop - The best place for your pets</p>
+    </div>
+    <div class="social-links d-flex flex-column align-items-center w-50">
+        <a href="https://example.com/facebook" class="text-decoration-none fw-medium"><i class="fab fa-facebook px-2"></i>Our Facebook</a>
+        <a href="https://example.com/twitter" class="text-decoration-none fw-medium"><i class="fab fa-twitter px-2"></i>Our Twitter</a>
+        <a href="https://example.com/instagram" class="text-decoration-none fw-medium"><i class="fab fa-instagram px-2"></i>Our Instagram</a>
+    </div>
+</footer>
