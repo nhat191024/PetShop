108712e45e36df190c0aef35bc88ddd435aa7baf
Fumino The Fish <richberchannel01@gmail.com>
2024-04-19 00:37:12 +0700

    Update: add session user id

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -11,19 +11,21 @@ function loginAuth($username, $password)
 {
     $email = accountGetByEmail($username);
     $user = accountGetByUsername($username);
-    if(!$email && !$user){
+    if (!$email && !$user) {
         header('Location: /?loginFailed=true');
-    } else if($password == $email['password'] || $password == $user['password']){
-            $_SESSION['loggedIn'] = true;
-            if($user){
-                $_SESSION['username'] = $user['username'];
-                $_SESSION['role'] = $user['role'];
-            } else {
-                $_SESSION['username'] = $email['username'];
-                $_SESSION['role'] = $email['role'];
-            }
-            header('Location: /');
+    } else if ($password == $email['password'] || $password == $user['password']) {
+        $_SESSION['loggedIn'] = true;
+        if ($user) {
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['role'] = $user['role'];
         } else {
-            header('Location: /?loginFailed=true');
+            $_SESSION['username'] = $email['username'];
+            $_SESSION['user_id'] = $email['id'];
+            $_SESSION['role'] = $email['role'];
         }
+        header('Location: /');
+    } else {
+        header('Location: /?loginFailed=true');
+    }
 }
