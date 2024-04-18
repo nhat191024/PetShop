67d02790cf9f31e3fc95f3fa094904cd93bb4ce3
Fumino The Fish <richberchannel01@gmail.com>
2024-04-19 00:45:20 +0700

    Update: remove some unnecessary link and new href

--- src/views/components/NavBar.php
+++ src/views/components/NavBar.php
@@ -1,6 +1,6 @@
     <nav class="navbar navbar-expand-lg bg-info bg-gradient fs-5 fw-bold px-5">
         <div class="container-fluid">
-            <a class="navbar-brand " href="#">
+            <a class="navbar-brand " href="/">
                 <img src="/assets/PetShop.svg" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,16 +11,13 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Product</a>
-                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Pet Category
                         </a>
                         <ul class="dropdown-menu text-center">
                             <?php foreach ($listPetCategory as $x) : ?>
-                                <li><a class="dropdown-item" href="#"><?= $x['name'] ?></a></li>
+                                <li><a class="dropdown-item" href="?view=petCategory&category=<?= $x['id'] ?>"><?= $x['name'] ?></a></li>
                                 <hr class="dropdown-divider">
                             <?php endforeach; ?>
                         </ul>
@@ -31,16 +28,10 @@
                         </a>
                         <ul class="dropdown-menu text-center">
                             <?php foreach ($listProductCategory as $x) : ?>
-                                <li><a class="dropdown-item" href="#"><?= $x['name'] ?></a></li>
+                                <li><a class="dropdown-item" href="?view=productCategory&category=<?= $x['id'] ?>"><?= $x['name'] ?></a></li>
                                 <hr class="dropdown-divider">
                             <?php endforeach; ?>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Introduce</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
                 <div class="d-flex gap-3 ">
@@ -59,8 +50,14 @@
                                 </li>
                                 <?php if ($_SESSION['role'] == '1') : ?>
                                     <li><a class="dropdown-item" href="/admin">Admin Dashboard</a></li>
-                                <?php endif;
-                            } else { ?>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                <?php endif; ?>
+                                <li>
+                                    <a class="dropdown-item" href="/controllers/logout.php">Logout</a>
+                                </li>
+                            <?php } else { ?>
                                 <li>
                                     <a type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#LoginModal">
                                         SignIn
@@ -69,18 +66,20 @@
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
-                                <li><a class="dropdown-item" href="#">SignUp</a></li>
+                                <li>
+                                    <a type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#SignUpModal">
+                                        SignUp
+                                    </a>
+                                </li>
                             <?php } ?>
                         </ul>
                     </div>
-                    <div class="dropdown">
-                        <button class="btn btn-info bg-gradient border border-2 border-black" style="width:60px;" data-bs-toggle="dropdown">
-                            <i class="fa-solid fa-cart-shopping" style="color: #000000;"></i>
-                        </button>
-                        <ul class="dropdown-menu text-center mt-1">
-                            <li>cart item</li>
-                        </ul>
-                    </div>
+                    <button type="button" class="btn btn-info bg-gradient border border-2 border-black position-relative" data-bs-toggle="modal" data-bs-target="#CartModal">
+                        <i class="fa-solid fa-cart-shopping" style="color: #000000;"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
+                            <?= $cart ? count($cart) : '0'  ?>+
+                        </span>
+                    </button>
                     <div class="dropstart">
                         <button class="btn btn-info bg-gradient border border-2 border-black" style="width:60px;" data-bs-toggle="dropdown">
                             <i class="fa-solid fa-calendar-days" style="color: #000000;"></i>
@@ -93,7 +92,3 @@
             </div>
         </div>
     </nav>
-
-    <?php
-    require_once "./views/components/LoginForm.php";
-    ?>
