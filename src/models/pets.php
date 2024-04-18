334749a05fb6b8ac3df93c5a26fd7c0dd3e759b8
Fumino The Fish <richberchannel01@gmail.com>
2024-04-19 00:42:26 +0700

    Update: get pet by category function

--- src/models/pets.php
+++ src/models/pets.php
@@ -1,17 +1,25 @@
 <?php
 require_once "pdo.php";
-function getPetByCategoryId($id) {
-    $sql = "SELECT * FROM pet WHERE category_id = $id"; 
-    return pdo_query($sql); 
+function getPetByCategoryId($id)
+{
+    $sql = "SELECT * FROM pet WHERE category_id = $id";
+    return pdo_query($sql);
 }
 
-function getPetById($id) {
+function getPetById($id)
+{
     $sql = "SELECT * FROM pet WHERE id = $id";
     return pdo_query_one($sql);
 }
 
-function getAllPet() {
+function getAllPet()
+{
     $sql = "SELECT * FROM pet";
     return pdo_query($sql);
 }
-?>
+
+function getAllPetByCategoryId($id)
+{
+    $sql = "SELECT * FROM pet WHERE category_id = $id";
+    return pdo_query($sql);
+}
