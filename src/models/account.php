5cc127fafa791e1dc5bca9d6eb5027abded83607
Fumino The Fish <richberchannel01@gmail.com>
2024-04-19 00:41:47 +0700

    Update: get account by id function & create account for signUp

--- src/models/account.php
+++ src/models/account.php
@@ -1,12 +1,25 @@
-<?php 
+<?php
 require_once "pdo.php";
-function accountGetByUsername($username ){
-    $sql = "SELECT * FROM account WHERE username = '$username'" ;
+function accountGetByUsername($username)
+{
+    $sql = "SELECT * FROM account WHERE username = '$username'";
     return pdo_query_one($sql);
 }
 
-function accountGetByEmail($email){
+function accountGetByEmail($email)
+{
     $sql = "SELECT * FROM account WHERE email = '$email'";
     return pdo_query_one($sql);
 }
-?>
+
+function getAccountById($id)
+{
+    $sql = "SELECT * FROM account WHERE id = $id";
+    return pdo_query_one($sql);
+}
+
+function accountCreate($username, $email, $password, $role)
+{
+    $sql = "INSERT INTO account (username, email, password, role) VALUES ('$username', '$email', '$password', '$role')";
+    return pdo_execute($sql);
+}
