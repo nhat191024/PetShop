4f8490178cbcca79aaeae061c1c2683d483d3d82
Fumino The Fish <richberchannel01@gmail.com>
2024-04-24 16:58:04 +0700

    Update: add view for case user click buy now

--- src/controllers/payment.php
+++ src/controllers/payment.php
@@ -1,15 +1,24 @@
 <?php
-if($view != 'payment' || empty($cart) || !isset($_SESSION['loggedIn'])) {
-    header('Location: /');
+if ($view != 'payment' || !isset($_SESSION['loggedIn'])) {
+    header('Location: /?notLogin=true');
     exit();
 } else {
-    require_once "./models/cart.php";
-    require_once "./models/product.php";
-    require_once "./models/productCategory.php";
-    require_once "./models/manufacturer.php";
-    require_once "./models/account.php";
+    if (isset($_GET['id'])) {
+        require_once "./models/product.php";
+        require_once "./models/productCategory.php";
+        require_once "./models/manufacturer.php";
+        require_once "./models/account.php";
+        $product = getProductById($_GET['id']);
+        require_once "./views/payment.php";
+    } else {
+        require_once "./models/cart.php";
+        require_once "./models/product.php";
+        require_once "./models/productCategory.php";
+        require_once "./models/manufacturer.php";
+        require_once "./models/account.php";
 
-    $cart = getAllUserCart($_SESSION['user_id']);
+        $cart = getAllUserCart($_SESSION['user_id']);
 
-    require_once "./views/payment.php";
+        require_once "./views/payment.php";
+    }
 }
