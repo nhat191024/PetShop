6327098af5b95b14a3afb37e06e9c3dbd985a49a
Fumino The Fish <richberchannel01@gmail.com>
2024-04-26 03:24:36 +0700

    Refactor

--- src/views/components/NavBar.php
+++ src/views/components/NavBar.php
@@ -69,7 +69,7 @@
                                     <hr class="dropdown-divider">
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="/controller/account/logout.php">Logout</a>
+                                    <a class="dropdown-item" href="/controllers/account/logout.php">Logout</a>
                                 </li>
                             <?php } else { ?>
                                 <li>
