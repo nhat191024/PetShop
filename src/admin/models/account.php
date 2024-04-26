2771f651287c70d0082b43c03874928cedbfdc6c
Fumino The Fish <richberchannel01@gmail.com>
2024-04-26 13:41:21 +0700

    Update: add get account by page & account update function

--- src/admin/models/account.php
+++ src/admin/models/account.php
@@ -12,3 +12,22 @@ function getAccountById($id)
     $sql = "SELECT * FROM account WHERE id = $id";
     return pdo_query_one($sql);
 }
+
+function getAccountByPage($page, $pageSize)
+{
+    $firstIndex = ($page - 1) * $pageSize;
+    $sql = "SELECT * FROM account LIMIT $firstIndex, $pageSize";
+    return pdo_query($sql);
+}
+
+function accountUpdate($id, $username, $email, $role, $avatar)
+{
+    $sql = "UPDATE account SET username = '$username', email = '$email', role = $role, avatar = '$avatar' WHERE id = $id";
+    pdo_execute($sql);
+}
+
+function accountUpdateStatus($id, $status)
+{
+    $sql = "UPDATE account SET status = $status WHERE id = $id";
+    pdo_execute($sql);
+}
