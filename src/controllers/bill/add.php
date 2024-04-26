9e8299a49202f0b77adb477c5bca2ab3d4d42fd5
Fumino The Fish <richberchannel01@gmail.com>
2024-04-26 13:45:18 +0700

    Update: now product and pet will change quantity if user use "Buy now"

--- src/controllers/bill/add.php
+++ src/controllers/bill/add.php
@@ -6,7 +6,9 @@ if (!isset($_SESSION['user_id'])) {
     require_once "../../models/bill.php";
     require_once "../../models/cart.php";
     require_once "../../models/product.php";
+    require_once "../../models/pets.php";
 
+    $type = $_POST['type'];
     $userId = $_POST['userId'];
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -20,15 +22,34 @@ if (!isset($_SESSION['user_id'])) {
     $payment = $_POST['payment'];
     $shipment = $_POST['shipment'];
 
+    $single = "";
+    if (isset($_POST['single'])) {
+        $single = $_POST['single'];
+    };
+
     $finalAddress = $address . ', ' . $ward . ', ' . $district . ', ' . $city;
 
-    $billId = createBill($userId, $name, $email, $total, $phone, $finalAddress, $note, $payment, $shipment);
-    $cart = getAllUserCart($userId);
-    foreach ($cart as $item) {
-        updateCartBill($item['id'], $billId);
+    $billId = createBill($type, $userId, $name, $email, $total, $phone, $finalAddress, $note, $payment, $shipment);
 
-        $product = getProductById($item['product_id']);
-        updateProductQuantity($product['id'], $product['quantity'] - $item['quantity']);
+    if ($single == "") {
+        $cart = getAllUserCart($userId);
+        foreach ($cart as $item) {
+            updateCartBill($item['id'], $billId);
+
+            $product = getProductById($item['product_id']);
+            updateProductQuantity($product['id'], $product['quantity'] - $item['quantity']);
+        }
+    } else {
+        if (isset($_POST['productId'])) {
+            $productId = $_POST['productId'];
+            updateProductQuantity($productId, getProductById($productId)['quantity'] - 1);
+        };
+
+        if (isset($_POST['petId'])) {
+            $petId = $_POST['petId'];
+            updatePetStock($petId, 0);
+        };
     }
+
     header('location: /?success=true');
 }
